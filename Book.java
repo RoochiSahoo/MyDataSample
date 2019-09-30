a88877ba23dc3ef41d2ffefb11680f531d2babf4
RoochiSahoo <55914216+RoochiSahoo@users.noreply.github.com>
2019-09-30 12:02:42 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,24 @@
+class Book
+{
+private int bookId;
+private String name;
+public void setbookId(int bookId)
+{ 
+this.bookId=bookId;
+system.out.println("book id is set");
+}
+public void setname(int bookId)
+{ 
+this.bookId=bookId;
+}
+public int getbookId()
+{ 
+return bookId;
+}
+public String getname()
+{ 
+return name;
+}
+
+
+}
